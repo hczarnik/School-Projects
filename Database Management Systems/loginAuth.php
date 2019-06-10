12a8b2dc9b9acbca58fe558b22112870b0e619ef
hczarnik <hczarnik20@gmail.com>
2019-06-10 15:25:12 -0400

    Update loginAuth.php

--- Database Management Systems/loginAuth.php
+++ Database Management Systems/loginAuth.php
@@ -4,56 +4,52 @@
 	Spring 2017
 -->
 <?php
-include "dbconfig.php";
-$busername=$_POST["username"];
-$bpassword=$_POST["password"];
+include "dbconfig.php";		//Includes file with information regarding connection to database
+$busername=$_POST["username"];	//Retrieves username that user enters on login page
+$bpassword=$_POST["password"];	//Retrieves password that user enters on login page
 
 $con = mysqli_connect($server,$login,$password,$dbname);
-$checkname = "SELECT id from CPS3740.Users where login = '$busername'";
-$result1 = mysqli_query($con,$checkname);
-$row1 = mysqli_fetch_array($result1);
-$count = mysqli_num_rows($result1);
+$checkname = "SELECT id from CPS3740.Users where login = '$busername'";	//Query to get the ID for the user's entered name
+$result1 = mysqli_query($con,$checkname);				//Query is run
+$row1 = mysqli_fetch_array($result1);					//Array with results from the query is stored in variable
+$count = mysqli_num_rows($result1);					//Number of rows from the query result is stored in variable
 
-if($count == 0){
-	print "<a href=\"http://(Website removed)/loginPage.php\">Return to Project 1 homepage</a>";
+if($count == 0){	//If the number of rows is 0, no results were found, exit the program because the user does not exist
+	print "<a href=\"http://(Website removed)/loginPage.php\">Return to Project 1 homepage</a>";	//Redirect user to homepage
 	echo "<br>";
 	die("Login ID " .$busername ." does not exist in the database.");
 }
 
+//Username exists, check if password matches for the verified username
 $checkpassword = "SELECT id from CPS3740.Users where login = '$busername' and BINARY password = BINARY '$bpassword'";
 $result2 =mysqli_query($con,$checkpassword);
 $row2 = mysqli_fetch_array($result2);
 $count2 = mysqli_num_rows($result2);
 
-if($count2 == 0){
-	print "<a href=\"http://(Website removed)/loginPage.php\">Return to Project 1 homepage</a>";
+if($count2 == 0){	//If the number of rows is 0, no results were found, exit the program because the user does not exist
+	print "<a href=\"http://(Website removed)/loginPage.php\">Return to Project 1 homepage</a>";	//Redirect user to homepage
 	echo "<br>";
 	die("User exists, but password does not match.");
 }
 
 //Username and password is now verified, print their IP, determine if they're at Kean University or not, then print the name, role and address from the user's credentials.
-
+//This part of code retrieves and examines the client's IP address
 if (!empty($_SERVER['HTTP_CLIENT_IP']))
-
 { $ip = $_SERVER['HTTP_CLIENT_IP']; }
-
 elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-
 { $ip = $_SERVER['HTTP_X_FORWARDED_FOR']; }
-
 else { $ip = $_SERVER['REMOTE_ADDR']; }
-
 $IPv4= explode(".",$ip);
+echo "<br>Your IP: $ip\n";	//Prints out the user's IP address
 
-echo "<br>Your IP: $ip\n";
-
+//If the user's IP address has certain prefixes, they are using Kean University's internet
 if (($IPv4[0] == 10) or ($IPv4[0] . "." . $IPv4[1] == "131.125") )
-
 { echo "<br>You are from Kean University.\n"; }
-
+//If the user's IP address does not have certain prefixes, they are not from Kean University
 else { echo "<br>You are NOT from Kean University.\n"; }
 echo "<br>";
 
+//Gets user's first and last name, which is known from the username that was entered in the login page
 $getname = "select concat(first_name,' ',last_name) as Name from CPS3740.Users where login = '$busername'";
 $nameResult = mysqli_query($con,$getname);
 if ($nameResult->num_rows > 0) {
@@ -64,6 +60,7 @@ if ($nameResult->num_rows > 0) {
 }
 echo "<br>";
 
+//Gets user's role (can be Manager, Employee, etc.) and displays it on the website
 $getrole = "select role from CPS3740.Users where login = '$busername'";
 $roleResult = mysqli_query($con,$getrole);
 if ($roleResult->num_rows > 0) {
@@ -74,6 +71,7 @@ if ($roleResult->num_rows > 0) {
 }
 echo "<br>";
 
+//Gets user's address from the database and displays it on the website
 $getaddress = "select concat(address, ', ', state, ', ', zipcode) as userAddress from Users where login= '$busername'";
 $addressResult = mysqli_query($con,$getaddress);
 if($addressResult->num_rows > 0){
@@ -84,6 +82,7 @@ if($addressResult->num_rows > 0){
 }
 ?>
 
+//HTML code to create a table with customer data
 <html>
 	<head>
 		<title></title>
